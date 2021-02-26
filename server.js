f3494eb5414f3ec8a5b8999e8fc6d2eea49a3eb5
Ricky Aston <astonmedia@hotmail.com>
2021-02-27 06:51:03 +1000

    backend logic for front end

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 const port = process.env.PORT || 8000;
 
+// Load body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.set("view engine", "ejs");
+
+// Load static assets
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // Home route
 app.get("/", (req, res) => {
